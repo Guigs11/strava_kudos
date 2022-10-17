79e8d90fadbfabbcec2c75ac9fb9f409be9cc945
Guigs11 <81813507+Guigs11@users.noreply.github.com>
2022-10-17 15:54:35 +0200

    Add files via upload

---
+++ strava_kudos_medium.py
@@ -0,0 +1,162 @@
+import pandas as pd
+import requests
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.ticker import MaxNLocator
+
+def get_token():
+    response = requests.post(
+                        url = 'https://www.strava.com/oauth/token',
+                        data = {
+                                'client_id': '[YOUR_CLIENT_ID]',
+                                'client_secret': '[YOUR_CLIENT_SECRET]',
+                                'refresh_token': '[YOUR_REFRESH_TOKEN]',
+                                'code': '[YOUR_AUTHORIZATION_CODE]',
+                                'grant_type': 'refresh_token'
+                                }
+                    )
+    access_token = response.json()['access_token']
+    print(access_token)
+    return access_token
+
+def get_activities(access_token, from_date, to_date):
+    url = f'https://www.strava.com/api/v3/athlete/activities?after={from_date}&before={to_date}&page=1&per_page=200'
+    payload={}
+    headers = {
+        'Authorization': 'Bearer ' + access_token
+        }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    activities = response.json()
+    return activities
+
+def get_kudos(access_token, activity_id):
+    url = f'https://www.strava.com/api/v3/activities/{activity_id}/kudos?page=1&per_page=200'
+    payload={}
+    headers = {
+        'Authorization': 'Bearer ' + access_token
+        }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    return data
+
+def transform(access_token, activities):
+    for act in activities:
+        if act["private"] == False:
+            activity_id = act['id']
+            nb_kudos = act['kudos_count']
+            kudos_list = [item['firstname'] + ' ' + item['lastname']  for item in get_kudos(access_token, activity_id)]
+
+            stats = {
+                'activity_id' : activity_id,
+                'nb_kudos' : nb_kudos,
+                'kudos_list': kudos_list
+            }
+            statlist.append(stats)
+    return
+
+def to_1D(series):
+    return pd.Series([x for list in series for x in list]) #series of lists to list of elements
+
+def plot(data):
+    # Setup plot size
+    fig, ax = plt.subplots(figsize=(20,6))
+
+    # Plot data
+    sns.barplot(x=data.index, y=data.values, palette='magma', zorder=2)
+
+    # Create grid 
+    ax.grid(which="major", axis='x', color='#DAD8D7', alpha=0.5, zorder=1)
+    ax.grid(which="major", axis='y', color='#DAD8D7', alpha=0.5, zorder=1)
+
+    # Remove splines
+    ax.spines[['top','right','bottom']].set_visible(False)
+
+    # Make left spine slightly thicker
+    ax.spines['left'].set_linewidth(1.1)
+
+    # Add label on top of each bar
+    rects = ax.patches
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2, height + 1, int(height), ha='center', va='bottom', size=8)  
+
+    # Add in red line and rectangle on top
+    ax.plot([0.12, .9],                  # Set width of line
+        [.98, .98],                  # Set height of line
+        transform=fig.transFigure,   # Set location relative to plot
+        clip_on=False, 
+        color='#E3120B', 
+        linewidth=.6)
+    ax.add_patch(plt.Rectangle((0.12,.98),                 # Set location of rectangle by lower left corder
+                        0.04,                       # Width of rectangle
+                        -0.02,                      # Height of rectangle. Negative so it goes down.
+                        facecolor='#E3120B', 
+                        transform=fig.transFigure, 
+                        clip_on=False, 
+                        linewidth = 0))
+
+    # Reformat x-axis tick labels
+    ax.xaxis.set_tick_params(labelsize=11)        # Set tick label size
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+
+    # Reformat yaxis
+    ax.yaxis.set_major_formatter(lambda s, i : f'{s:,.0f} kudos')
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_tick_params(pad=-2,             # Pad tick labels so they don't go over y-axis
+                        labeltop=True,      # Put x-axis labels on top
+                        labelbottom=False,  # Set no x-axis labels on bottom
+                        bottom=False,       # Set no ticks on bottom
+                        labelsize=11)       # Set tick label size                  
+
+    # Add in title and subtitle
+    ax.text(x=0.12, y=.91, s="Strava Kudoers", transform=fig.transFigure, ha='left', fontsize=13, weight='bold', alpha=.8)
+    ax.text(x=0.12, y=.87, s="Number of kudos given from 01/01/2022 to 31/12/2022", transform=fig.transFigure, ha='left', fontsize=11, alpha=.8)
+    
+    # Adjust the spaces and margins of the plots
+    plt.subplots_adjust(left=None, bottom=0.2, right=None, top=0.85, wspace=None, hspace=None)
+    
+    # Export plot as high resolution PNG
+    plt.savefig('strava_kudos.png', dpi = 300, bbox_inches="tight", facecolor='white', pad_inches=0.5)
+    
+    plt.show()
+    
+    return
+
+
+
+# Get access token
+access_token = get_token()
+
+# Define the from date and to date
+from_date = datetime(2022, 1, 1) #datetime(year, month, day)
+from_date = int(from_date.timestamp()) #epoch of from_date
+to_date = datetime(2022, 12, 31) #datetime(year, month, day)
+to_date = int(to_date.timestamp()) #epoch of to_date
+
+# Get activities in scope
+activities = get_activities(access_token, from_date, to_date)
+
+# Put the relevant data in a Dataframe
+statlist = []
+transform(access_token, activities)
+df = pd.DataFrame(statlist)
+print(df.head())
+
+# Get the number of activities in the period
+nb_activities = df['nb_kudos'].count()
+print(f'number of activities : {nb_activities}')
+
+# Get the number of kudos in the period
+nb_kudos = df['nb_kudos'].sum()
+print(f'number of kudos : {nb_kudos}')
+
+# Calculate the average number of kudos per activity
+avg_kudos_per_activity = round(df['nb_kudos'].mean(),2)
+print(f'average kudos per activity : {avg_kudos_per_activity}')
+
+# From the Dataframe get the list of top kudoers
+kudos = to_1D(df['kudos_list']).value_counts()
+
+# Plot the kudoers on a bar chart
+plot(kudos)
